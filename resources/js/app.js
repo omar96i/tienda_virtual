70d0ed2d943f89d36ca65e6c7998bab8ecd05f31
omar96i <sagara9930@hotmail.com>
2021-02-08 15:45:45 -0500

    tratando

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = new Vue({
+    el: '#app',
+    components:{
+        ExampleComponent
+    }
+});
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('template.template_main')
+
+@section('content')
+<div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi cupiditate odit explicabo unde doloribus commodi amet ad facilis natus mollitia non totam laborum possimus, ea ipsa dicta sunt repellat quibusdam.</p>
+</div>
+@endsection
